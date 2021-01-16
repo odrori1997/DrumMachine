a144d4ac10cf2bdd0ce5f3eb495654cd7843c4e9
Omer Drori <odrori574@gmail.com>
2021-01-16 17:10:33 -0500

    Added HTML5 audio element to play audio

--- drum-machine/src/Components/Controller.js
+++ drum-machine/src/Components/Controller.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import Pad from './Pad.js';
+import soundfile1 from '../assets/c# minior jam track 12.12.2020.mp3';
+import soundfile2 from '../assets/will this be the end of us loop.mp3';
+import soundfile3 from '../assets/jam track 12.21.2020.mp3';
+import soundfile4 from '../assets/jam track 1.2.2021.mp3';
+import soundfile5 from '../assets/jam 1.3.2021.mp3';
+import soundfile6 from '../assets/jam track 1.6.2021.mp3';
+import soundfile7 from '../assets/jam track 12.21.2020.mp3';
+import soundfile8 from '../assets/ig jam track in d 11.2.2020.mp3';
+import soundfile9 from '../assets/can\'t say goodbye.mp3';
+
 
 export default class Controller extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            pads: Array(9).fill(null)
+            pads: Array(9).fill(false)
         };
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.soundfiles = [new Audio(soundfile1), new Audio(soundfile2), new Audio(soundfile3), new Audio(soundfile4), new Audio(soundfile5), 
+            new Audio(soundfile6), new Audio(soundfile7), new Audio(soundfile8), new Audio(soundfile9)];
     }
 
     componentDidMount() {
@@ -18,87 +30,68 @@ export default class Controller extends Component {
         document.removeEventListener("keydown", this.handleKeyPress, false);
     }
 
+    //  todo:
+    // fix bootstrap to be in a proper grid.
     handleKeyPress(e) {
+        console.log(e.keyCode + " pressed");
         // "q" button pressed
         if (e.keyCode === 81) {
-            let newPads = this.state.pads;
-            newPads[0] = true;
-            this.setState({
-                pads: newPads
-            });
+            this.play(0);
         }
         // "w" button pressed
         if (e.keyCode === 87) {
-            let newPads = this.state.pads;
-            newPads[1] = true;
-            this.setState({
-                pads: newPads
-            });
+            this.play(1);
         }
         // "e" button pressed
         if (e.keyCode === 69) {
-            let newPads = this.state.pads;
-            newPads[2] = true;
-            this.setState({
-                pads: newPads
-            });
+            this.play(2);
         }
         // "a" button pressed
         if (e.keyCode === 65) {
-            let newPads = this.state.pads;
-            newPads[3] = true;
-            this.setState({
-                pads: newPads
-            });
+           this.play(3);
         }
         // "s" button pressed
         if (e.keyCode === 83) {
-            let newPads = this.state.pads;
-            newPads[4] = true;
-            this.setState({
-                pads: newPads
-            });
+           this.play(4);
         }
         // "d" button pressed
         if (e.keyCode === 68) {
-            let newPads = this.state.pads;
-            newPads[5] = true;
-            this.setState({
-                pads: newPads
-            });
+            this.play(5);
         }
         // "z" button pressed
         if (e.keyCode === 90) {
-            let newPads = this.state.pads;
-            newPads[6] = true;
-            this.setState({
-                pads: newPads
-            });
+           this.play(6);
         }
         // "x" button pressed
         if (e.keyCode === 88) {
-            let newPads = this.state.pads;
-            newPads[7] = true;
-            this.setState({
-                pads: newPads
-            });
+            this.play(7);
         }
         // "d" button pressed
         if (e.keyCode === 67) {
-            let newPads = this.state.pads;
-            newPads[8] = true;
-            this.setState({
-                pads: newPads
-            });
+            this.play(8);
         }
-        // TODO:
-        // learn how to play audio through react
+    }
+
+    play = (id) => {
+        console.log("Pad " + this.props.ID + " is playing.");
+        if (!this.state.pads[id]) {
+            this.soundfiles[id].play();
+        }
+        else {
+            this.soundfiles[id].pause();
+        }
+        let newPads = this.state.pads;
+        newPads[id] = !this.state.pads[id];
+        this.setState({
+            pads: newPads
+        });
     }
 
     render() {
         let pads = [];
         for (let i = 0; i < 9; i++) {
-            pads.push(<Pad key={i} isPlaying={this.state.pads[i]} />)
+            console.log("Pushing Pad with ID " + i + " with isPlaying prop " + this.state.pads[i]);
+            pads.push(<Pad key={i} ID={i} isPlaying={this.state.pads[i]} audio={this.soundfiles[i]} />)
         }
         return (
             <div class="container">
--- drum-machine/src/Components/Pad.js
+++ drum-machine/src/Components/Pad.js
@@ -1,17 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
 
 export default class Pad extends Component {
     constructor(props) {
         super(props);
         this.state = {
             isPlaying: this.props.isPlaying,
-            // audio: this.props.audio
+            showEdit: false
         };
+        this.showEditFields = this.showEditFields.bind(this);
     }
+
+    showEditFields() {
+        let divID = "editFields" + this.props.ID;
+        let showEditFields = document.getElementById(divID);
+        if (!this.state.showEdit) {
+            showEditFields.style.display = "block";
+        }
+        else {
+            showEditFields.style.display = "none";
+        }
+        this.setState({
+            showEdit: !this.state.showEdit
+        });
+    }
+
+//     <div id="editFields0" style={{'display':'none'}}>
+//     <label htmlFor="audio-file-1" class="custom-file-upload">
+//         <IoIosAddCircle /> Choose audio file
+//     </label>
+//     <br /><br />
+//     <input id="audio-file-1" type="file"/>
+//     <audio controls loop id="audio1" type="audio/mpeg-3" src={soundfile1} >
+//     </audio>
+// </div>
+
+
     render() {
+        let textColor = this.state.isPlaying ? "text-warning" : "text-secondary";
         return (
-            <div class="col-4 bg-primary">
-                <h1 class="text-secondary">Pad {this.props.key}</h1>
+            <div class="col-4 bg-primary" onClick={this.showEditFields}>
+                <h1 class={textColor}>Pad {this.props.ID}</h1>
             </div>
         )
     }
